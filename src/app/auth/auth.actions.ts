aab0f36798c34768d387e07554cc334819118d4d
AlexL70 <alexander.levinson.70@gmail.com>
2019-10-02 16:04:15 +0300

    11. NgRx Reducers - Step-by-Step Implementation

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,4 +1,4 @@
-import { User } from './../model/user.model';
+import { User } from './model/user.model';
 import { createAction, props } from '@ngrx/store';
 
 export const login = createAction(
--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,15 +1,29 @@
+import { AuthState } from './index';
+import { User } from './../model/user.model';
 import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  createReducer,
+  on
 } from '@ngrx/store';
+import { AuthActions } from '../action.types';
 
-export interface AppState {
-
+export interface AuthState {
+  user: User;
 }
 
-export const reducers: ActionReducerMap<AppState> = {
-
+export const initialAuthState: AuthState = {
+  user: undefined
 };
+
+export const authReducer = createReducer(initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return { user: action.user };
+  }),
+  on(AuthActions.logout, (state, action) => {
+    return { user: undefined };
+  })
+);
